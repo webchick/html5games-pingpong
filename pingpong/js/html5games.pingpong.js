1ee3eed6820060dfc414c878f912c8960a0562db
webchick <webchick@24967.no-reply.drupal.org>
2012-05-11 20:43:29 -0700

    Paddle B! It liiiiives.

--- pingpong/js/html5games.pingpong.js
+++ pingpong/js/html5games.pingpong.js
@@ -1,4 +1,27 @@
+var KEY = {
+  UP: 38,
+  DOWN: 40,
+  W: 87,
+  S: 83
+}
+
 $(function() {
+  // Listen for the key down event.
+  $(document).keydown(function(e) {
+    switch(e.which) {
+      case KEY.UP: // arrow-up
+        // Get the current paddle B's top value.
+        var top = parseInt($("#paddleB").css("top"));
+        // Move the paddle B up 5 pixels.
+        $("#paddleB").css("top", top - 5);
+        break;
+      case KEY.DOWN: // arrow-down
+        var top = parseInt($("#paddleB").css("top"));
+        // Move the paddle B down 5 pixels.
+        $("#paddleB").css("top", top + 5);
+    }
+  });
+
   $("#paddleA").css("top", "60px");
   $("#paddleB").css("top", "20px");
 });
